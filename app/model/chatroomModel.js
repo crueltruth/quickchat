a81e411a09ecd8698848cdb3048f8a293ab708f8
crueltruth <262773167@qq.com>
2014-05-09 01:47:15 +0800

    app目录

---
+++ app/model/chatroomModel.js
@@ -0,0 +1,15 @@
+Model.chatroomModel = function(exports) {
+	exports.config = {
+		fields : [{
+			name : 'username',
+			type : 'string'
+		}, {
+			name : 'message',
+			type : 'string'
+		}, {
+			name : 'time',
+			type : 'datetime',
+			defaultValue : 'now()'
+		}]
+	};
+};
